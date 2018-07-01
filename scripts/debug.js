e65d42d5def6cf9f1aa206b83d1721df7e48f10a
Diego Acosta <diegonake@gmail.com>
2018-07-01 21:10:42 +0200

    create scripts folder

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import fs from 'fs';
-import Nes from './lib/nes';
+import Nes from '../lib/nes';
 
 const nes = new Nes();
 nes.loadCart(fs.readFileSync('./roms/dk.nes'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: './scripts/debug.js',
   target: 'node',
   mode: 'development',
   output: {
