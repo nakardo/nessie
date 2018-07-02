392a7fc9fc67b0baeffad38febf0a7caa35a8d4c
Diego Acosta <diegonake@gmail.com>
2018-07-02 18:50:57 +0200

    add babel-node back for debugging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const Nes = require('./dist/bundle.node');
+const Nes = require('./src/nes');
 
 const nes = new Nes();
 nes.loadCart(fs.readFileSync('./roms/dk.nes'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,10 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/nes.js',
-  target: 'node',
   mode: 'development',
   output: {
-    filename: 'bundle.node.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new CleanWebpackPlugin(['dist'])
@@ -18,24 +16,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: ['node_modules'],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['babel-preset-env', {
-                targets: {
-                  node: 'current'
-                }
-              }]
-            ],
-            plugins: [
-              'transform-strict-mode',
-              'transform-object-rest-spread',
-              'transform-class-properties',
-              'add-module-exports'
-            ]
-          }
-        }
+        use: { loader: 'babel-loader' }
       }
     ]
   }
