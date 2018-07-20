9f13ff0ca9108097716301290dd4cf168dd38c42
Diego Acosta <diegonake@gmail.com>
2018-07-20 16:00:39 +0200

    fix isc opcode

--- src/6502/instruction-set.js
+++ src/6502/instruction-set.js
@@ -24,7 +24,7 @@ const execute = [
   bcs, lda, stp, lax, ldy, lda, ldx, lax, clv, lda, tsx, las, ldy, lda, ldx, lax,
   cpy, cmp, nop, dcp, cpy, cmp, dec, dcp, iny, cmp, dex, axs, cpy, cmp, dec, dcp,
   bne, cmp, stp, dcp, nop, cmp, dec, dcp, cld, cmp, nop, dcp, nop, cmp, dec, dcp,
-  cpx, sbc, nop, isc, cpx, sbc, inc, inc, inx, sbc, nop, sbc, cpx, sbc, inc, isc,
+  cpx, sbc, nop, isc, cpx, sbc, inc, isc, inx, sbc, nop, sbc, cpx, sbc, inc, isc,
   beq, sbc, stp, isc, nop, sbc, inc, isc, sed, sbc, nop, isc, nop, sbc, inc, isc,
 ];
 
