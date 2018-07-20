36c485f7c36527a6bf9b80b4d3ec235cd5f7bcc8
Diego Acosta <diegonake@gmail.com>
2018-07-20 16:06:39 +0200

    update passing list

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const nes = new Nes();
 // Passing:
 // - 01-basics.nes
 // - 02-implied.nes
+// - 03-immediate.nes
+// - 04-zero_page.nes
+// - 05-zp_xy.nes
+// - 06-absolute.nes
 // - 10-branches.nes
 //
 // Failing:
-// - 03-immediate.nes (0x01)
-// - 04-zero_page.nes (0x01)
-// - 05-zp_xy.nes (0x01)
-// - 06-absolute.nes (0x01)
 // - 07-abs_xy.nes (Unimplemented opcode: 0x02)
 // - 08-ind_x.nes (UnmappedAddressError: 0xa5ff is an unknown address)
 // - 09-ind_y.nes (UnmappedAddressError: 0xffff is an unknown address)
-nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/03-immediate.nes'));
+nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/07-abs_xy.nes'));
 nes.start();
