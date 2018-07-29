9f77b741c0d55b1e660ac98268967a825281090a
Diego Acosta <diegonake@gmail.com>
2018-07-29 23:32:25 +0200

    all single tests passes

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const fs = require('fs');
 const Nes = require('./src/nes');
 
 const nes = new Nes();
-// Passing:
-// - 01-basics.nes
-// - 02-implied.nes
-// - 03-immediate.nes
-// - 04-zero_page.nes
-// - 05-zp_xy.nes
-// - 06-absolute.nes
-// - 07-abs_xy.nes
-// - 08-ind_x.nes
-// - 09-ind_y.nes
-// - 10-branches.nes
-// - 11-stack.nes
-// - 12-jmp_jsr.nes
-// - 13-rts.nes
-// - 14-rti.nes
-// - 15-brk.nes
-// - 16-special.nes
-nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/08-ind_x.nes'));
+// Tests:
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/01-basics.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/02-implied.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/03-immediate.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/04-zero_page.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/05-zp_xy.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/06-absolute.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/07-abs_xy.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/08-ind_x.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/09-ind_y.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/10-branches.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/11-stack.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/12-jmp_jsr.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/13-rts.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/14-rti.nes'));
+// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/15-brk.nes'));
+nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/16-special.nes'));
 nes.start();
