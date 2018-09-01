132d54e12abe7efe10a6a29adda2eec647c7d0b8
Diego Acosta <diegonake@gmail.com>
2018-09-01 18:38:33 +0200

    cleanup mapper creation

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -7,13 +7,14 @@ export default class Cart {
   mapper = null;
 
   constructor(data) {
-    const mapper = (data[6] >> 4) | (data[7] & 0xf0);
-
     debug('ROM Control Byte #1: %s', data[6].to(2));
     debug('ROM Control Byte #2: %s', data[7].to(2));
-    debug('Mapper #: %d', mapper);
 
-    this.mapper = new MAPPERS[mapper](data);
+    const index = (data[6] >> 4) | (data[7] & 0xf0);
+    const Mapper = MAPPERS[index];
+    debug('mapper idx: %d, class: %s', index, Mapper.name);
+
+    this.mapper = new Mapper(data);
   }
 
   r8(addr) {
