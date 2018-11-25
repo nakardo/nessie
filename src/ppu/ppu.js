f2cc7ec8692bea50ada0256533d5e3a9857441d0
Diego Acosta <diegonake@gmail.com>
2018-11-26 00:15:59 +0100

    add ppu registers logic

--- src/ppu/ppu.js
+++ src/ppu/ppu.js
@@ -11,16 +11,26 @@ const PPUCTRL_IGNORE_WRITES_CYCLES = 30000;
 export default class Ppu {
   cpu = null;
 
+  // Registers
+
   ctrl = 0;
+  mask = 0;
   stat = 0;
-  oamData = 0;
-  ppuData = 0;
+  oamAddr = 0;
+  scroll = [0, 0]; // (x, y)
+  ppuAddr = 0;
 
   t = 0;
   scanline = 0;
   resetCycles = 0;
+  writeCount = 0;
   reset = true;
   latch = 0;
+
+  // Memory
+
+  oam = new Uint8Array(0x100);
+  vram = new Uint8Array(0x4000);
 
   // 8 kbytes rom or ram on cart + mappers
   // 2 kbytes ram in the console
@@ -33,6 +43,8 @@ export default class Ppu {
   step(cycles) {
     this.t += cycles * 3;
 
+    // After power/reset, writes to this register are ignored for about
+    // 30,000 cycles.
     if (this.reset) {
       this.resetCycles += cycles;
       if (this.resetCycles > PPUCTRL_IGNORE_WRITES_CYCLES) {
@@ -57,6 +69,12 @@ export default class Ppu {
       // pre-render line.
       this.stat |= 1 << 7;
     }
+
+    // OAMADDR is set to 0 during each of ticks 257-320 (the sprite tile
+    // loading interval) of the pre-render and visible scanlines.
+    if (this.scanline >= 257 && this.scanline <= 320) {
+      this.oamAddr = 0;
+    }
   }
 
   r8(addr) {
@@ -64,22 +82,31 @@ export default class Ppu {
 
     switch (0x2000 | (addr & 0x7)) {
       case PPU.PPUSTATUS:
-        return (this.latch = this.stat);
+        // Reading the status register will clear bit 7 mentioned above and
+        // also the address latch used by PPUSCROLL and PPUADDR. It does not
+        // clear the sprite 0 hit or overflow bit.
+        this.stat &= ~(1 << 7);
+        this.latch = this.stat;
+        break;
       case PPU.OAMDATA:
-        return (this.latch = this.oamData);
+        // TODO: Writes will increment OAMADDR after the write; reads during
+        // vertical or forced blanking return the value from OAM at that
+        // address but do not increment.
+        this.latch = this.oam[this.oamAddr];
+        break;
       case PPU.PPUDATA:
-        return (this.latch = this.ppuData);
+        this.latch = this.ppuData;
+        break;
       case PPU.PPUCTRL:
       case PPU.PPUMASK:
       case PPU.OAMADDR:
       case PPU.PPUSCROLL:
       case PPU.PPUADDR:
-        return this.latch;
       default:
         break;
     }
 
-    throw new UnmappedAddressError(addr);
+    return this.latch;
   }
 
   w8({val, addr}) {
@@ -100,10 +127,34 @@ export default class Ppu {
           this.cpu.nmi = true;
         }
         return;
-      case PPU.PPUSTATUS:
-        // TODO: After power/reset, writes to this register are ignored for
-        // about 30,000 cycles.
+      case PPU.PPUMASK:
+        this.mask = val;
         break;
+      case PPU.OAMADDR:
+        this.oamAddr = val;
+        break;
+      case PPU.OAMDATA:
+        this.oam[this.oamAddr++] = val;
+        this.oamAddr &= 0xff;
+        break;
+      case PPU.PPUSCROLL:
+        // Horizontal offsets range from 0 to 255. "Normal" vertical offsets
+        // range from 0 to 239, while values of 240 to 255 are treated as -16
+        // through -1 in a way, but tile data is incorrectly fetched from the
+        // attribute table.
+        this.scroll[this.writeCount++] = val;
+        this.writeCount &= 1;
+        break;
+      case PPU.PPUADDR: {
+        const offset = this.writeCount << 2;
+        this.ppuAddr &= ~(0xf << offset);
+        this.ppuAddr |= val << offset;
+        this.writeCount++;
+        this.writeCount &= 1;
+        break;
+      }
+      case PPU.PPUDATA:
+        return this.vram[this.ppuAddr & 0x3fff];
       default:
         break;
     }
