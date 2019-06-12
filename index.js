6587a3ab792650339b1cefbc3c0fc051b5fc49a3
Diego Acosta <diegonake@gmail.com>
2019-06-12 19:05:14 +0200

    update format

--- index.js
+++ index.js
@@ -21,12 +21,10 @@ const nes = new Nes();
 // nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/13-rts.nes'));
 // nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/14-rti.nes'));
 // nes.loadCart(fs.readFileSync('./roms/instr_test-v5/rom_singles/15-brk.nes'));
-nes.loadCart(
-  fs.readFileSync('./roms/instr_test-v5/rom_singles/16-special.nes'),
-);
+// nes.loadCart(fs.readFilqeSync('./roms/instr_test-v5/rom_singles/16-special.nes'));
 //
 // All tests:
-// nes.loadCart(fs.readFileSync('./roms/instr_test-v5/official_only.nes'));
+nes.loadCart(fs.readFileSync('./roms/instr_test-v5/official_only.nes'));
 //
 // Roms:
 // nes.loadCart(fs.readFileSync('./roms/excitebike.nes'));
