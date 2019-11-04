b7103d268051c71d389fb47a4ff1dc97d7924330
Diego Acosta <diegonake@gmail.com>
2019-11-05 00:29:28 +0100

    code cleanup, revert offset on 32kb mode

--- src/cart/mappers/mmc1.js
+++ src/cart/mappers/mmc1.js
@@ -37,27 +37,44 @@ export default class MMC1 {
 
   getChrRxmBank(index) {
     if (this.chrRxmBankMode == 0) {
-      const bank = this.chrRxmBank[0] * 2;
-      if (index == 0) return bank;
-      return bank | 1;
+      if (index == 0) return this.chrRxmBank[0] & ~1;
+      else return this.chrRxmBank[0] | 1;
+    } else {
+      return this.chrRxmBank[index];
     }
-
-    return this.chrRxmBank[index];
   }
 
   getPrgRomBank(index) {
     const mode = this.prgRomBankMode;
     if (mode == 0 || mode == 1) {
-      const bank = (this.prgRomBank >> 1) * 2;
-      if (index == 0) return bank;
-      return bank | 1;
-    } else if (mode == 2 && index == 0) {
-      return 0;
-    } else if (mode == 3 && index == 1) {
-      return this.prgRomLastPage;
+      if (index == 0) return this.prgRomBank & ~1;
+      else return this.prgRomBank | 1;
+    } else if (mode == 2) {
+      if (index == 0) return 0;
+      else return this.prgRomBank;
+    } else if (mode == 3) {
+      if (index == 0) return this.prgRomBank;
+      else return this.prgRomLastPage;
     }
+  }
 
-    return this.prgRomBank;
+  writeRegister(addr) {
+    const hnib = addr >> 12;
+    debug('write register: %s, val: %s', hnib.to(16), this.shift.to(2));
+    if (addr < 0xa000) {
+      debug('set control: %s', this.shift.to(2));
+      this.mirroring = this.shift & 3;
+      this.prgRomBankMode = (this.shift >> 2) & 3;
+      this.chrRxmBankMode = (this.shift >> 4) & 1;
+    } else if (addr < 0xe000) {
+      const index = (addr >> 14) & 1;
+      this.chrRxmBank[index] = this.shift & 0x1f;
+      debug('set bank chr-rxm[%d]: %d', index, this.shift);
+    } else {
+      this.prgRamEnable = (this.shift & 0x10) == 0;
+      this.prgRomBank = this.shift & 0xf;
+      debug('set bank prg-rom: %d', this.prgRomBank);
+    }
   }
 
   w8({val, addr}) {
@@ -65,37 +82,20 @@ export default class MMC1 {
       const bank = this.getChrRxmBank((addr >> 12) & 1);
       this.chrRxm[bank][addr & 0xfff] = val;
     } else if (addr < 0x6000) {
-      return 0;
+      return;
     } else if (addr < 0x8000) {
       this.prgRam[addr & 0x1fff] = val;
     } else {
       if (val & 0x80) {
-        debug('set control |= 0xc');
+        debug('reset control');
         this.prgRomBankMode = 3;
         this.shiftReset();
       } else if (this.shiftCount < 4) {
         this.shiftRight(val);
-      } else if (this.shiftCount === 4) {
-        const hnib = addr >> 12;
-        this.shiftRight(val);
-        debug('set register: %s, val: %s', hnib.to(16), this.shift.to(2));
-        if (addr < 0xa000) {
-          debug('set control: %s', this.shift.to(2));
-          this.mirroring = this.shift & 3;
-          this.prgRomBankMode = (this.shift >> 2) & 3;
-          this.chrRxmBankMode = (this.shift >> 4) & 1;
-        } else if (addr < 0xe000) {
-          const index = addr < 0xc000 ? 0 : 1;
-          this.chrRxmBank[index] = this.shift;
-          debug('set bank chr-rxm[%d]: %d', index, this.shift);
-        } else {
-          this.prgRamEnable = (this.shift & 0x10) == 0;
-          this.prgRomBank = this.shift & 0xf;
-          debug('set bank prg-rom: %d', this.prgRomBank);
-        }
-        this.shiftReset();
       } else {
-        throw new Error('unhandled register write');
+        this.shiftRight(val);
+        this.writeRegister(addr);
+        this.shiftReset();
       }
     }
   }
